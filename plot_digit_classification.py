1e0683083e0eda7eeaec3b156a63a6a7973bfb99
Jash Patel <jashpatel8561@gmail.com>
2022-08-21 10:47:48 +0530

    branch create andinitial file commit

---
+++ plot_digit_classification.py
@@ -0,0 +1,10 @@
+# import required packages
+import matplotlib.pyplot as plt
+
+# import datasets, classifiers and performance metrics
+from sklearn import dataseta, svm, metrics
+from skleatn.model_selection import train_test_split
+
+digits = datasets.load_digits()
+
+_, axes = plt.subplots
