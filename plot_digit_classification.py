4eabdffd714320d68e82d08760941f417350fbd0
Jash Patel <jashpatel8561@gmail.com>
2022-09-16 11:28:39 +0530

    identify ML stages of code

--- plot_digit_classification.py
+++ plot_digit_classification.py
@@ -1,3 +1,4 @@
+# PART: library dependencies: -- sklearn, tenserflow, numpy
 # import required packages
 import matplotlib.pyplot as plt
 
@@ -5,29 +6,67 @@ import matplotlib.pyplot as plt
 from sklearn import datasets, svm, metrics
 from sklearn.model_selection import train_test_split
 
+
+GAMMA = 0.001
+train_frac = 0.8
+test_frac = 0.1
+dev_frac = 0.1
+
+
+# PART: -load dataset --data from files, csv, tsv, json, pickle
 digits = datasets.load_digits()
 
+
+# PART: sanity check visulization of data
 _, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
 for ax, image, label in zip(axes, digits.images, digits.target):
     ax.set_axis_off()
     ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
     ax.set_title("Training: %i" % label)
     
-    
+
+# PART: data pre-processing -- to normlize data, to remove noice,
+#                               formate the data tobe consumed by model
 n_samples = len(digits.images)
 data = digits.images.reshape((n_samples, -1))
 
-# Create a classifier: a support vector classifier
-clf = svm.SVC(gamma=0.001)
 
-# Split data into 50% train and 50% test subsets
-X_train, X_test, y_train, y_test = train_test_split(
-    data, digits.target, test_size=0.5, shuffle=False
+# PART: define train/dev/test splite of experiment protocol
+# trin to train model
+# dev to set hyperperameter of the model
+# test to evaluate the performane of the model
+
+# 80:10:10 train:dev:test
+
+dev_test_frac = 1- train_frac
+X_train, X_dev_test, y_train, y_dev_test = train_test_split(
+    data, digits.target, test_size=dev_test_frac, shuffle=True
 )
 
+
+fraction_want = dev_frac/(dev_frac+test_frac)
+X_test, X_dev, y_test, y_train = train_test_split(
+    x_dev_test, y_dev_test, test_size=fraction_want, shuffle=True
+)
+# if testing on the same as traning set: the performance matrics may overestimate 
+# the goodness of the model. 
+# We want to test on "unseen" sample. 
+
+
+# PART: Define the model
+# Create a classifier: a support vector classifier
+clf = svm.SVC()
+
+# PART: setting up hyper parameter
+hyper_param = {'gamma': GAMMA}
+clf.set_param(**hyper_param)
+
+# PART: train model
 # Learn the digits on the train subset
 clf.fit(X_train, y_train)
 
+
+# PART: get test set pridection
 # Predict the value of the digit on the test subset
 predicted = clf.predict(X_test)
 
@@ -39,7 +78,7 @@ for ax, image, prediction in zip(axes, X_test, predicted):
     ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
     ax.set_title(f"Prediction: {prediction}")
     
-    
+# PART: Compute evaluation Matrics 
 print(
     f"Classification report for classifier {clf}:\n"
     f"{metrics.classification_report(y_test, predicted)}\n"
