f772b77659f63646d5a830931b6fb53319f987ae
Jash Patel <jashpatel8561@gmail.com>
2022-09-16 20:31:07 +0530

    find best hyper param for best accuracy

--- plot_digit_classification.py
+++ plot_digit_classification.py
@@ -5,9 +5,23 @@ import matplotlib.pyplot as plt
 # import datasets, classifiers and performance metrics
 from sklearn import datasets, svm, metrics
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+# 1. set the range of hyperparameters
+gamma_list  = [0.01, 0.005, 0.001, 0.0005, 0.0001]
+c_list = [0.1, 0.2, 0.5, 0.7, 1, 2, 5, 7, 10]
 
 
-GAMMA = 0.001
+# 2. for every combiation of hyper parameter values
+hyp_para_combo = [{"gamma":g, "C":c} for g in gamma_list for c in c_list]
+
+assert len(hyp_para_combo) == len(gamma_list)*len(c_list)
+
+
+# model_hyperparams
+# GAMMA = 0.001
+# C = 0.5
+
 train_frac = 0.8
 test_frac = 0.1
 dev_frac = 0.1
@@ -52,33 +66,54 @@ X_test, X_dev, y_test, y_dev = train_test_split(
 # the goodness of the model. 
 # We want to test on "unseen" sample. 
 
+best_hyp_param = None
+best_model = None
+accuracy = 0
+for curr_param in hyp_para_combo:
+    # PART: Define the model
+    # Create a classifier: a support vector classifier
+    clf = svm.SVC()
 
-# PART: Define the model
-# Create a classifier: a support vector classifier
-clf = svm.SVC()
+    # PART: setting up hyper parameter
+    hyper_param = curr_param
+    clf.set_params(**hyper_param)
 
-# PART: setting up hyper parameter
-hyper_param = {'gamma': GAMMA}
-clf.set_params(**hyper_param)
-
-# PART: train model
-# Learn the digits on the train subset
-clf.fit(X_train, y_train)
+    # PART: train model
+    # 2a. train the model
+    # Learn the digits on the train subset
+    clf.fit(X_train, y_train)
 
 
-# PART: get test set pridection
-# Predict the value of the digit on the test subset
-predicted = clf.predict(X_test)
+    # PART: get test set pridection
+    # Predict the value of the digit on the test subset
+    curr_predicted = clf.predict(X_test)
 
 
+    # 2b. compute accuracy on validation set
+    curr_accuracy = accuracy_score(y_test, curr_predicted)
+
+    # 3. identify best set of hyper parameter for which validation set acuuracy is highest
+    if accuracy < curr_accuracy:
+        best_hyp_param = hyper_param
+        accuracy = curr_accuracy
+        best_model = clf
+        print(f"{best_hyp_param} \tAccuracy: {accuracy}")
+    
+
+
+# if predicted < curr_predicted:
+#     predicted = curr_predicted
+predicted = best_model.predict(X_test) 
+
+# PART: sanity check visulization of data
 _, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
-for ax, image, prediction in zip(axes, X_test, predicted):
+for ax, image, prediction in zip(axes, X_test, curr_predicted):
     ax.set_axis_off()
     image = image.reshape(8, 8)
     ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
     ax.set_title(f"Prediction: {prediction}")
-    
 # PART: Compute evaluation Matrics 
+# 4. report the best set accuracy with that best model.
 print(
     f"Classification report for classifier {clf}:\n"
     f"{metrics.classification_report(y_test, predicted)}\n"
