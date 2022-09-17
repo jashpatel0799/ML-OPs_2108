e83390e1bd16079eb089fdec23a1ffd262fd24e5
Jash Patel <jashpatel8561@gmail.com>
2022-09-17 17:24:31 +0530

    best hyperparam and c for diffferent size of image dataset

--- plot_digit_classification.py
+++ plot_digit_classification.py
@@ -1,11 +1,14 @@
 # PART: library dependencies: -- sklearn, tenserflow, numpy
 # import required packages
 import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
 
 # import datasets, classifiers and performance metrics
 from sklearn import datasets, svm, metrics
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+from skimage.transform import rescale, resize
 
 # 1. set the range of hyperparameters
 gamma_list  = [0.01, 0.005, 0.001, 0.0005, 0.0001]
@@ -30,91 +33,108 @@ dev_frac = 0.1
 # PART: -load dataset --data from files, csv, tsv, json, pickle
 digits = datasets.load_digits()
 
-
 # PART: sanity check visulization of data
 _, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
 for ax, image, label in zip(axes, digits.images, digits.target):
+    print(f"image size: {image.shape}")
     ax.set_axis_off()
     ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
     ax.set_title("Training: %i" % label)
-    
-
+# exit()    
 # PART: data pre-processing -- to normlize data, to remove noice,
 #                               formate the data tobe consumed by model
-n_samples = len(digits.images)
-data = digits.images.reshape((n_samples, -1))
 
+n = len(digits.images)
 
-# PART: define train/dev/test splite of experiment protocol
-# trin to train model
-# dev to set hyperperameter of the model
-# test to evaluate the performane of the model
-
-# 80:10:10 train:dev:test
-
-dev_test_frac = 1- train_frac
-X_train, X_dev_test, y_train, y_dev_test = train_test_split(
-    data, digits.target, test_size=dev_test_frac, shuffle=True
-)
-
-
-fraction_want = dev_frac/(dev_frac+test_frac)
-X_test, X_dev, y_test, y_dev = train_test_split(
-    X_dev_test, y_dev_test, test_size=fraction_want, shuffle=True
-)
-# if testing on the same as traning set: the performance matrics may overestimate 
-# the goodness of the model. 
-# We want to test on "unseen" sample. 
-
-best_hyp_param = None
-best_model = None
-accuracy = 0
-for curr_param in hyp_para_combo:
-    # PART: Define the model
-    # Create a classifier: a support vector classifier
-    clf = svm.SVC()
-
-    # PART: setting up hyper parameter
-    hyper_param = curr_param
-    clf.set_params(**hyper_param)
-
-    # PART: train model
-    # 2a. train the model
-    # Learn the digits on the train subset
-    clf.fit(X_train, y_train)
-
-
-    # PART: get test set pridection
-    # Predict the value of the digit on the test subset
-    curr_predicted = clf.predict(X_test)
-
-
-    # 2b. compute accuracy on validation set
-    curr_accuracy = accuracy_score(y_test, curr_predicted)
-
-    # 3. identify best set of hyper parameter for which validation set acuuracy is highest
-    if accuracy < curr_accuracy:
-        best_hyp_param = hyper_param
-        accuracy = curr_accuracy
-        best_model = clf
-        print(f"{best_hyp_param} \tAccuracy: {accuracy}")
+# best_hyp_param = None
+# best_model = None
+# accuracy = 0
+best_size = 0
+size = [4, 16, 32]
+_, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
+for s in size:
+    print(f'Image: {(s,s)}')
+    new_digits = np.empty((n, s, s))
+    for image in digits.images:
+        image_resized = resize(image, (s, s), anti_aliasing=True)
+        np.append(new_digits, image_resized)
     
 
+    n_samples = len(new_digits)
+    data = digits.images.reshape((n_samples, -1))
 
-# if predicted < curr_predicted:
-#     predicted = curr_predicted
-predicted = best_model.predict(X_test) 
 
-# PART: sanity check visulization of data
-_, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
-for ax, image, prediction in zip(axes, X_test, curr_predicted):
-    ax.set_axis_off()
-    image = image.reshape(8, 8)
-    ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
-    ax.set_title(f"Prediction: {prediction}")
-# PART: Compute evaluation Matrics 
-# 4. report the best set accuracy with that best model.
-print(
-    f"Classification report for classifier {clf}:\n"
-    f"{metrics.classification_report(y_test, predicted)}\n"
-) 
+    # PART: define train/dev/test splite of experiment protocol
+    # trin to train model
+    # dev to set hyperperameter of the model
+    # test to evaluate the performane of the model
+
+    # 80:10:10 train:dev:test
+
+    dev_test_frac = 1- train_frac
+    X_train, X_dev_test, y_train, y_dev_test = train_test_split(
+        data, digits.target, test_size=dev_test_frac, shuffle=True
+    )
+
+
+    fraction_want = dev_frac/(dev_frac+test_frac)
+    X_test, X_dev, y_test, y_dev = train_test_split(
+        X_dev_test, y_dev_test, test_size=fraction_want, shuffle=True
+    )
+    # if testing on the same as traning set: the performance matrics may overestimate 
+    # the goodness of the model. 
+    # We want to test on "unseen" sample.
+
+    best_hyp_param = None
+    best_model = None
+    accuracy = 0
+
+    for curr_param in hyp_para_combo:
+        # PART: Define the model
+        # Create a classifier: a support vector classifier
+        clf = svm.SVC()
+
+        # PART: setting up hyper parameter
+        hyper_param = curr_param
+        clf.set_params(**hyper_param)
+
+        # PART: train model
+        # 2a. train the model
+        # Learn the digits on the train subset
+        clf.fit(X_train, y_train)
+
+
+        # PART: get test set pridection
+        # Predict the value of the digit on the test subset
+        curr_predicted = clf.predict(X_test)
+
+
+        # 2b. compute accuracy on validation set
+        curr_accuracy = accuracy_score(y_test, curr_predicted)
+
+        # 3. identify best set of hyper parameter for which validation set acuuracy is highest
+        if accuracy < curr_accuracy:
+            best_size = image_resized.shape
+            best_hyp_param = hyper_param
+            accuracy = curr_accuracy
+            best_model = clf
+            print(f"{best_hyp_param} \tAccuracy: {accuracy} \tsize: {best_size}")
+        
+
+    # if predicted < curr_predicted:
+    #     predicted = curr_predicted
+    predicted = best_model.predict(X_test) 
+
+    # PART: sanity check visulization of data
+    _, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
+    for ax, image, prediction in zip(axes, X_test, curr_predicted):
+        ax.set_axis_off()
+        image = image.reshape(8, 8)
+        ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
+        ax.set_title(f"Prediction: {prediction}")
+    # PART: Compute evaluation Matrics 
+    # 4. report the best set accuracy with that best model.
+    print(
+        f"Classification report for classifier {clf}:\n"
+        f"{metrics.classification_report(y_test, predicted)}\n"
+    )
